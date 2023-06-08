832d77fdb8cd336f57f0024612a61bd30a0ed8fb
dania-aam <daashrafmohamed@up.edu.ph>
2023-06-08 12:15:43 +0800

    deployment edit

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from src import create_app, db
 #from waitress import serve
 
 # Create DB and run
-db.create_all(app=create_app())
+#db.create_all(app=create_app())
 
 
 # Run app only
-#app=create_app()
+app=create_app()
 
 
